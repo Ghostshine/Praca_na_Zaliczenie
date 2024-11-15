d7b6d12f41c4c631fdefa5c7d416e2a72c8c3a66
Ghostshine <daria7660d.b@gmail.com>
2024-11-15 22:12:20 +0100

    Initial commit

--- GuessTheNumber/GuessTheNumber/GameController.cs
+++ GuessTheNumber/GuessTheNumber/GameController.cs
@@ -16,28 +16,28 @@ namespace GuessTheNumber
         public GameController()
         {
             Random random = new Random();
-            getNumber = random.Next(1, 101);
+            getNumber = random.Next ( 1, 101 );
             Count = 0;
         }
 
-        public string CheckAnswer(int userAnswer)
+        public string CheckAnswer( int userAnswer )
         {
             Count++;
-            if (userAnswer < getNumber)
+            if ( userAnswer < getNumber )
             {
                 return "\nZa mało\n";
             }
-            else if (userAnswer > getNumber)
+            else if ( userAnswer > getNumber )
             {
                 return "\nZa dużo\n";
             }
             else
             {
-                return $"\n\nGratulacje! Odgadłeś liczbę {getNumber} w {Count} próbach.";
+                return $"\n\nGratulacje! Odgadłeś liczbę { getNumber } w { Count } próbach.";
             }
         }
 
-        public bool IsCorrect(int userGuess)
+        public bool IsCorrect( int userGuess )
         {
             return userGuess == getNumber;
         }
--- GuessTheNumber/GuessTheNumber/Program.cs
+++ GuessTheNumber/GuessTheNumber/Program.cs
@@ -3,49 +3,50 @@ using System.Timers;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main( string[] args )
     {
-        Console.WriteLine("-------------------------------------------");
-        Console.WriteLine("Zasady:\n" +
+        bool keepRunning = true;
+
+        Console.WriteLine( "-------------------------------------------\n" +
+            "Zasady:\n" +
                           "\t1. Liczba zostanie wylosowana od 1 do 1000\n" +
                           "\t2. Twoim zadaniem jest odgadnięcie liczby\n" +
-                          "\t3. Za mało - twoja liczba jest mniejsza || Za dużo - twoja liczba jest większa");
-        Console.WriteLine("-------------------------------------------");
+                          "\t3. Za mało - twoja liczba jest mniejsza || Za dużo - twoja liczba jest większa\n" +
+                          "-------------------------------------------" );
 
 
-        while (true)
+        while ( keepRunning )
         {
-            Console.WriteLine("Losować? (T/N)");
-            string answer = Console.ReadLine().Trim().ToUpper();
+            Console.WriteLine( "Losować? (T/N)" );
+            string firstAnswer = Console.ReadLine().Trim().ToUpper();
 
-                if (answer == "T")
+                if ( firstAnswer == "T" )
                 {
                     GameController game = new GameController();
                     int userAnswer = 0;
                     game.Clear();
 
-                while (!game.IsCorrect(userAnswer))
+                while ( !game.IsCorrect( userAnswer ) )
                     {
-                        Console.Write("Wpisz swoją odpowiedź: ");
+                        Console.Write( "Wpisz swoją odpowiedź: " );
 
-                        if (int.TryParse(Console.ReadLine(), out userAnswer))
+                        if ( int.TryParse( Console.ReadLine(), out userAnswer ) )
                         {
-                            string result = game.CheckAnswer(userAnswer);
-                            Console.WriteLine(result);
+                            string result = game.CheckAnswer( userAnswer );
+                            Console.WriteLine( result );
                         }
                         else
                         {
-                            Console.WriteLine("Nieprawidłowa wartość. Wpisz liczbę całkowitą.");
+                            Console.WriteLine( "Nieprawidłowa wartość. Wpisz liczbę całkowitą." );
                         }
                 }
             }
-                else if (answer == "N")
+                else if ( firstAnswer == "N" )
                 {
-                    Console.WriteLine("Okej...");
-                    
-                    Environment.Exit(50);
+                    keepRunning = false;
+                    Environment.Exit( 0 );
                 }
-                else Console.WriteLine("Niepoprawna odpowiedź. Wpisz 'T' aby rozpocząć nową grę, lub 'N' aby zakończyć.");
+                else Console.WriteLine( "Niepoprawna odpowiedź. Wpisz 'T' aby rozpocząć nową grę, lub 'N' aby zakończyć." );
 
             
         }
